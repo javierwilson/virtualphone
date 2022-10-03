1ede699dddff53301f3ad247e6f75d8f0c898d77
javier wilson <javier.wilson@viamo.io>
2022-10-02 22:51:42 -0600

    adding singleton support to manage settings from admin

--- sms/models.py
+++ sms/models.py
@@ -2,6 +2,7 @@
 
 from django.db import models
 from django.template.defaultfilters import date
+from solo.models import SingletonModel
 
 
 class Message(models.Model):
@@ -16,3 +17,17 @@ class Message(models.Model):
 
     def __str__(self):
         return '%s %s %s' % (date(self.created_at,'d M Y'), self.message_from, self.message_body)
+
+
+class SiteConfiguration(SingletonModel):
+    site_name = models.CharField(max_length=255, default='Site Name')
+    maintenance_mode = models.BooleanField(default=False)
+    twilio_callerid = models.CharField(max_length=255)
+    email_to = models.CharField(max_length=255)
+    email_from = models.CharField(max_length=255)
+
+    def __str__(self):
+        return "Site Configuration"
+
+    class Meta:
+        verbose_name = "Site Configuration"
